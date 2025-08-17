9b1b541c635c94e19fd0eee63bd5d87221b28d56
Hasan Ali <76755570+Hasan-Alii@users.noreply.github.com>
2025-08-17 16:44:42 +0300

    [206] [Time Beats: 100.00%] [Memory Beats: 14.08%] - HasanPush

---
+++ Reverse-Linked-List.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while(cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
